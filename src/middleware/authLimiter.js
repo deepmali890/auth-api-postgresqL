b0be3f5312327bf6cb567f8cf4b3081b9f870198
deepmali890 <deepmali590@gmail.com>
2025-05-01 10:40:52 +0530

    tests

--- src/middleware/authLimiter.js
+++ src/middleware/authLimiter.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 module.exports = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 10,
-  message: 'Too many attempts, try again after 15 minutes',
+  message: 'Too many attempts, try again after 15 minutes ',
   standardHeaders: true,
   legacyHeaders: false,
 });
